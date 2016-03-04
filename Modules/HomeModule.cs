b1586f1ae00c8a3b8a368940ffb253a94e53353d
sea413 <seanmp42@gmail.com>
2016-03-04 11:56:29 -0800

    Add home views to allow users to enter input into the database.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,38 +1,48 @@
-// using System;
-// using System.Collections.Generic;
-// using Nancy;
-// using Nancy.ViewEngines.Razor;
-//
-// namespace Registrar
-// {
-//   public class HomeModule : NancyModule
-//   {
-//     public HomeModule()
-//     {
-//       Get["/"] = _ => {
-//         return View["index.cshtml"];
-//       };
-//
-//       Get["/students"] = _ => {
-//       List<Student> AllStudents = Student.GetAll();
-//       return View["students.cshtml", AllStudents];
-//       };
-//
-//       Get["/classes"] = _ => {
-//       List<Class> AllClasses = Class.GetAll();
-//       return View["classes.cshtml", AllClasses];
-//       };
-//
-//       Get["/class/{id}"] = parameters => {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Class selectedClass = Class.Find(parameters.id);
-//       List<Student> allStudents = Student.GetAll();
-//       List<Student> classStudents = selectedClass.GetStudents();
-//       model.Add("class", selectedClass);
-//       model.Add("allStudents", allStudents);
-//       model.Add("classStudents", classStudents);
-//       return View["class.cshtml", model];
-//       };
+using System;
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace Shoes
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+
+      Get["/stores"] = _ => {
+      List<Store> AllStores = Store.GetAll();
+      return View["students.cshtml", AllStores];
+      };
+
+      Get["/brands"] = _ => {
+      List<Brand> AllBrands = Brand.GetAll();
+      return View["brands.cshtml", AllBrands];
+      };
+
+      Get["/brand/{id}"] = parameters => {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Brand Selectedbrand = Brand.Find(parameters.id);
+      List<Store> allStores = Store.GetAll();
+      List<Store> BrandStores = selectedBrand.GetStudents();
+      model.Add("brand", Selectedbrand);
+      model.Add("allstores", allStores);
+      model.Add("brandstore", BrandStores);
+      return View["brand.cshtml", model];
+      };
+      Get["/store/{id}"] = parameters => {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Store SelectedStore = Store.Find(parameters.id);
+      List<Brand> allbrands = Brand.GetAll();
+      List<Brand> Storesbrands = SelectedStore.GetBrands();
+      model.Add("store", SelectedStore);
+      model.Add("allbrands", allbrands);
+      model.Add("storebrand", Storesbrands);
+      return View["store.cshtml", model];
+      };
 //
 //       Post["class/add_student"] = _ => {
 //       Class selectedClass = Class.Find(Request.Form["class-id"]);
@@ -55,6 +65,6 @@
 //       List<Class> AllClasses = Class.GetAll();
 //       return View["classes.cshtml", AllClasses];
 //       };
-//     }
-//   }
-// }
+    }
+  }
+}
